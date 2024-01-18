40323b1fd41618f78aabadb56acd045c2457525b
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-01-18 15:13:16 +0900

    予約完了画面のレイアウト変更

--- resources/views/shops/done.blade.php
+++ resources/views/shops/done.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <div class="flex items-center justify-center h-screen">
-        <div class="bg-white p-8 rounded-md shadow-md text-center md:w-4/12 w-full flex flex-col items-center justify-center">
+        <div
+            class="bg-white p-8 rounded-md shadow-md text-center md:w-4/12 md:w-6/12 w-full flex flex-col items-center justify-center">
             <h1 class="text-xl mb-6 tracking-widest">ご予約ありがとうございます</h1>
             <a href="{{ route('home') }}" class="bg-blue-500 text-white px-4 py-2 rounded-md mt-4 inline-block">戻る</a>
         </div>
