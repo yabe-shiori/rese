956664ba4010bd99ca027c7ea39ee9587e2dc6d6
yabe shiori <sh_0418_ty@yahoo.co.jp>
2024-02-03 17:47:32 +0900

    Disable automatic opening of browser in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
     server: {
+        open: false,
         hmr: {
-            host: 'localhost',
+            host: "localhost",
         },
-    }
+    },
 });
